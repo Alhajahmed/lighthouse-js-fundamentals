43aa20dd7d2355361936956de0052f806a544f84
Ahmed <ahmed91alhaj@gmail.com>
2023-01-18 12:23:38 -0500

    Create a function to determine if a number is odd -not working-

---
+++ odd.js
@@ -0,0 +1,9 @@
+public static boolean isOdd(int num) {
+    if(num % 2 == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+System.out.println(isOdd(3)); // true
+System.out.println(isOdd(4)); // false
